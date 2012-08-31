04db4f2042b2959bc350b7f0efd2027a211019ba
Kamil Załęski <kamil.zaleski@gmail.com>
2012-08-31 20:24:10 +0200

    CSRF protecion

--- lib/proxy_request.rb
+++ lib/proxy_request.rb
@@ -1,35 +1,55 @@
 # coding: utf-8
+
+require 'httpclient'
+
+def _proxy_request(env,path,args = {})
+
+  req = ActionDispatch::Request.new(env)
+
+  if not args[:skip_csrf] and env['rack.session']['_csrf_token'] != req.headers["X-CSRF-Token"]
+    return [
+      403,
+      {'Content-Type' => 'text/html', 'Content-Length' => '3'},
+      ['403']
+    ]
+  end
+
+  path_params = path.to_s % Hash[req.params.map{|a| [a.first.to_sym, a.last]}]
+
+  if path_params.starts_with?("http:/")
+    path_params.gsub!('http:/','http://')
+  end
+
+  if req.params['format']
+    path_params += "." + req.params['format']
+  end
+
+  uri = URI.parse( path_params )
+
+  http_req = HTTPClient.new
+
+  elapsed_time = Benchmark.ms do
+    @response = http_req.request(req.method,uri.to_s)
+  end
+
+  [
+    @response.status,
+    {
+      'Content-Type' => @response.header.contenttype
+    },
+    [@response.body.content]
+  ]
+
+end
+
 module ActionDispatch
   module Routing
     class Mapper
       module HttpHelpers
-      
-        def proxy_request(path)
 
+        def proxy_request(path,args = {})
           lambda do |env|
-
-            req = Request.new(env)
-
-            path_params = path.to_s % Hash[req.params.map{|a| [a.first.to_sym, a.last]}]
-
-            if path_params.starts_with?("http:/")
-              path_params.gsub!('http:/','http://')
-            end
-  
-
-            if req.params['format']
-              path_params += "." + req.params['format']
-            end
-
-            uri = URI.parse( path_params )
-
-            http_req = HTTPClient.new
-
-            elapsed_time = Benchmark.ms do
-              @response = http_req.request(req.method,uri.to_s)
-            end
-            
-            [ @response.status, {'Content-Type' => @response.header.contenttype }, [@response.body.content] ]
+            _proxy_request(env,path,args)
           end
         end
 
